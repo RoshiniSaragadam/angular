0a0915c94ba88d15340b6ab1bf6f62e4ec201863
Roshini <roshinisaragadam1@gmail.com>
2025-09-09 10:26:01 +0530

    change

---
+++ src/app/user1/user1.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user1',
+  imports: [],
+  templateUrl: './user1.html',
+  styleUrl: './user1.css'
+})
+export class User1 {
+
+}
