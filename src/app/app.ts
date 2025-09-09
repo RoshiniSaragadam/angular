5705edb53404eb97d2a974bc64489d0c8bd1c012
Roshini <roshinisaragadam1@gmail.com>
2025-09-09 13:55:41 +0530

    change2

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { User1 } from './user1/user1';
 
 @Component({
   selector: 'app-user1',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, User1],
   templateUrl: './user1.html',
   styleUrl: './user1.css'
 })
